4883a66d72f356df925389e430bb3ec8e0faaa67
helenathineos42 <101715310+helenathineos42@users.noreply.github.com>
2022-03-24 10:26:28 +0100

    Add files via upload

---
+++ GameFrame(1).java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+import javax.swing.ImageIcon;
+public class GameFrame extends JFrame {
+	ImageIcon logo = new ImageIcon(getClass().getClassLoader().getResource("snake.png"));
+	
+	GameFrame() {
+		
+		this.add(new GamePanel());
+		this.setTitle("Snake");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+		this.setIconImage(logo.getImage());
+
+	}
+
+}
